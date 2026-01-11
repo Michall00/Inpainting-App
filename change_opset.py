f600f2c9f4d545c32c888773ec937141755e7f58
misadowsk <michalsad.protondynamic@gmail.com>
2026-01-11 18:50:43 +0100

    delete unused models from assets

--- change_opset.py
+++
@@ -1,6 +0,0 @@
-import onnx
-from onnx import version_converter
-
-m = onnx.load("assets/coordfill.onnx")
-m = version_converter.convert_version(m, 12)
-onnx.save(m, "assets/coordfill_opset12.onnx")
--- lower_opset_version.py
+++
@@ -1,3 +0,0 @@
-import onnx
-
-print(onnx.load("assets/encoder_best_pruned_012_single_ir9.onnx").ir_version)
